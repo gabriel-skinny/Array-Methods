5770fd92edfcc975d8cb474abacfb6daef5f61f7
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2020-11-10 16:44:15 -0300

    reduce

---
+++ reduce.js
@@ -0,0 +1,20 @@
+
+Array.prototype.myReduce = function(callback, valorIncial){
+  let valorFinal = typeof valorIncial !== undefined ? valorIncial : this[0]
+
+  for(index in this){
+    valorFinal = callback(valorFinal, this[index], this)
+
+  }
+
+  return valorFinal;
+} 
+
+
+
+let teste = [1, 2, 3, 909]
+
+
+var resultadoTeste = teste.myReduce((anterior, proximo) =>  anterior + proximo, 0)
+
+console.log(resultadoTeste)
